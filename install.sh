433ed2f0dafd52e4d9747c541b7d4fa9752b43d0
Tonmoy Islam Tanvir <42781990+tonmoy-islam@users.noreply.github.com>
2019-08-31 01:14:52 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-
+#####variables#####
+dir='/data/data/com.termux/files/home/public-ip'
 #####colors#####
 
 red='\033[1;31m'
 green='\033[1;32m'
 blue='\033[1;36m'
 yellow='\033[1;33m'
-purple='\033[1;35m'
 
 #####Coding section#####
 echo -e "${green}[+]{yellow}Installing requirements..." 
 echo ""
-sleep 0.4s
+sleep 0.5s
 apt install figlet ruby -y
 clear
 echo -e "${purple}[*]${yellow}Setup running..."
-cd public-ip
+sleep 0.5s
+cd $dir
 chmod 770 pubip
 mv pubip $PREFIX/bin/
 echo -e "${green}[√]${blue}Setup complete."
+echo -e "${yellow}Thanks for installing. Have a good day. (^_^)
