59f1df46f741ca91c566fd8d3625703d2365c561
Tonmoy Islam Tanvir <42781990+tonmoy-islam@users.noreply.github.com>
2019-08-31 01:04:25 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-clear
+
+#####colors#####
+
+red='\033[1;31m'
+green='\033[1;32m'
+blue='\033[1;36m'
+yellow='\033[1;33m'
+purple='\033[1;35m'
+
+#####Coding section#####
+echo -e "${green}[+]{yellow}Installing requirements..." 
+echo ""
+sleep 0.4s
 apt install figlet ruby -y
+clear
+echo -e "${purple}[*]${yellow}Setup running..."
 cd public-ip
 chmod 770 pubip
 mv pubip $PREFIX/bin/
+echo -e "${green}[√]${blue}Setup complete."
