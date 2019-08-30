362825662f222659eba7e8b06ffce94f01ba2de6
Tonmoy Islam Tanvir <42781990+tonmoy-islam@users.noreply.github.com>
2019-08-31 02:17:26 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,17 +10,20 @@ blue='\033[1;36m'
 yellow='\033[1;33m'
 
 #####Coding section#####
-echo -e "${green}[+]{yellow}Installing requirements..." 
+echo -e "${green}[+] ${yellow}Installing requirements..." 
 echo ""
 sleep 1s
 apt install curl figlet -y
+echo -e "${green}[√] Succesfully installed requirements."
+sleep 2s
 clear
-echo -e "${purple}[*]${yellow}Setup running..."
+echo -e "${purple}[*] ${yellow}Setup running..."
+sleep 2s
 cd $dir
 chmod 770 pubip
 mv pubip $PREFIX/bin/
 cd
 mv public-ip/ $move
-sleep 2s
-echo -e "${green}[√]${blue}Setup complete. Just type- \033[1;42mpubip on your terminal"
+clear
+echo -e "${green}[√] ${blue}Setup complete. Just type- \033[0;47mpubip${blue} on your terminal"
 echo -e "${yellow}Thanks for installing. Have a good day. (^_^)"
